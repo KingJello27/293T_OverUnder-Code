ad9ea83f1e068ba8e51acfacca6e861066a4d7d4
KingJello27 <jellaadwait@gmail.com>
2023-09-06 17:46:02 -0400

    clamp control

--- src/comp/opcontrol.cpp
+++ src/comp/opcontrol.cpp
@@ -6,6 +6,8 @@ void opcontrol() {
   // This is preference to what you like to drive on.
   chassis.set_drive_brake(MOTOR_BRAKE_COAST);
 
+  bool clampState = true;
+
   while (true) {
 
     chassis.tank(); // Tank control
@@ -23,15 +25,24 @@ void opcontrol() {
     }
 
     //lift control
-    if (master.get_digital(E_CONTROLLER_DIGITAL_L2)){
+    if (master.get_digital(DIGITAL_L2)){
       lift.move_velocity(100);
-    }else if (master.get_digital(E_CONTROLLER_DIGITAL_L1)){
+    }else if (master.get_digital(DIGITAL_L1)){
       lift.move_velocity(-100);
     }else{
       lift.move_velocity(0);
     }
-    
 
+    //clamp control
+    // if (master.get_digital_new_press(DIGITAL_R1)){
+    //   leftWing.set_value(true);
+    //   rightWing.set_value(true);
+    // }
+
+    if (master.get_digital_new_press(DIGITAL_R1)){
+      clampState = !clampState;
+      clamp.set_value(clampState);
+    }
     // . . .
     // Put more user control code here!
     // . . .
